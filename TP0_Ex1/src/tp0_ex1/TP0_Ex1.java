705627f92e4d343634b6d39409ec710f8286281b
Pierce Broudin <piercebroudin.pb@gmail.com>
2021-10-08 15:23:12 +0200

    suppression de TPO_Ex1

---
+++ TP0_Ex1/src/tp0_ex1/TP0_Ex1.java
@@ -0,0 +1,30 @@
+/*
+TP0 Exercice 1
+Broudin
+DELIGNE
+*/
+package ex1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author piercebroudin
+ */
+public class Ex1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String prenom; 
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom?");
+        prenom = sc.nextLine();
+        System.out.println("Au revoir");
+        // TODO code application logic here
+        // ihougovy pi futodf cirxu
+    }
+
+}
