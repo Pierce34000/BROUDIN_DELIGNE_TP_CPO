e6d0fc338e5fa52377aec5d1697ee95115444dd7
Pierce Broudin <piercebroudin.pb@gmail.com>
2021-10-25 14:33:06 +0200

    Creation du projet

---
+++ TP1_Convertisseur_DELIGNE_BROUDIN/src/tp1_convertisseur_deligne_broudin/TP1_Convertisseur_DELIGNE_BROUDIN.java
@@ -0,0 +1,42 @@
+/*
+BROUDIN
+DELIGNE
+ */
+package tp1_convertisseur_deligne_broudin;
+
+/**
+ *
+ * @author piercebroudin
+ */
+public class TP1_Convertisseur_DELIGNE_BROUDIN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // temperature à convertir
+        
+        double temperature;
+        Scanner sr;
+        sr = new Scanner(System.in);
+        System.out.println("Bonjour, saisissez une valeur : ");
+        temperature = sr.nextFloat();
+        System.out.println("Saisissez la conversion que vous souhaitez effectuer");
+        
+        Scanner cr;
+        int option; 
+        cr = new Scanner(System.in);
+        System.out.println("1) De celcius vers Kelvin");
+        System.out.println("2) De Kelvin vers Celcius");
+        System.out.println("3) De Farenheit vers Celcius");
+        System.out.println("4) De Celcius vers Farenheit");
+        System.out.println("5) De Kelvin vers Farenheit");
+        System.out.println("6) De Farenheit vers Kelvin");
+        option = cr.nextInt();
+        
+        public static void CelciusVersKelvin(double )
+        
+    }
+    
+}
