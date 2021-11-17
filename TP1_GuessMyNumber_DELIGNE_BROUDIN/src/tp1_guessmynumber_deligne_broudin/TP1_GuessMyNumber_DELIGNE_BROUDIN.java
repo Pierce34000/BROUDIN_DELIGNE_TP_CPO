fd87b37b44244d3d646488f539fcabb6b1504d2c
Pierce Broudin <piercebroudin.pb@gmail.com>
2021-11-17 16:09:13 +0100

    fin

--- TP1_GuessMyNumber_DELIGNE_BROUDIN/src/tp1_guessmynumber_deligne_broudin/TP1_GuessMyNumber_DELIGNE_BROUDIN.java
+++ TP1_GuessMyNumber_DELIGNE_BROUDIN/src/tp1_guessmynumber_deligne_broudin/TP1_GuessMyNumber_DELIGNE_BROUDIN.java
@@ -16,7 +16,7 @@ public class TP1_GuessMyNumber_DELIGNE_BROUDIN {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args, int saisieUtilisateur) {
+    public static void main(String[] args) {
         // TODO code application logic here
         Random generateurAleat = new Random();
         // 1)
